425b9cf37a3fc037b88ec247935dda6afb5875b2
Brian Stack <bis12@case.edu>
2011-09-10 12:51:00 -0400

    Move layouts into views folder, and styles into public folder

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ gem 'sinatra', :git => "git://github.com/sinatra/sinatra.git"
 gem 'slim'           
 gem 'shotgun'
 gem 'thin'
+gem 'less'
--- sharematch.rb
+++ sharematch.rb
@@ -1,22 +1,7 @@
 require 'sinatra'
 require 'slim'
+require 'less'
 
 get '/' do
 	slim :index
 end
-
-__END__
-
-@@layout
-doctype html
-html
-  head
-    meta charset="utf-8"
-    title Share*Match
-    link rel="stylesheet" 
-  body
-    h1 Share*Match
-    == yield
-
-@@index
-h2 This is thin! Running with Shotgun!
