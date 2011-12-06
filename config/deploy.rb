5d8ebfa9174b4d6adb941c26ff9de150c5b248c5
Tom Dooner <tomdooner@gmail.com>
2011-12-05 19:56:26 -0500

    Commit Capistrano deploy file.
    I'm sick of this file being overwritten all the time. So it's committed now!

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,18 @@
-set :application, "Sharemat.ch"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
 
-set :scm, :subversion
+set :application, "sharemat.ch"
+set :repository,  "git://github.com/ted27/cwru-eecs341-project.git"
+
+set :scm, "git"
+set :user, "tom"
+set :branch, "master"
+set :deploy_to, "/var/www/html_tom/sharematch/"
+set :use_sudo, :false
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "sharemat.ch"                          # Your HTTP server, Apache/etc
+role :app, "sharemat.ch"                          # This may be the same as your `Web` server
+role :db,  "sharemat.ch", :primary => true # This is where Rails migrations will run
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
